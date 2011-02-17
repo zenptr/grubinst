10a52ef8f3b306e161957725eed27951bd7b788a
g@chenall.cn <g@chenall.cn@d95a8657-0e37-07c3-0af6-720afd5d0260>
2011-02-17 06:53:28 +0000

    vfont sources update
    git-svn-id: http://grubutils.googlecode.com/svn@52 d95a8657-0e37-07c3-0af6-720afd5d0260

--- trunk/src/vfont.c
+++ trunk/src/vfont.c
@@ -1,6 +1,5 @@
 #include "grub4dos.h"
-#define CHAR_BUF1 ((char *)0x7C30)
-#define CHAR_BUF2 ((char *)0x7E00)
+
 struct realmode_regs {
 	unsigned long edi; // as input and output
 	unsigned long esi; // as input and output
@@ -52,54 +51,73 @@ main ()
 		bl=字模集代号(有效值：0～7)   
 		int 10h
 	*/
-	unsigned char prog[40] = {
-				0xBA, 0xC0, 0x00, 0xBD, 0x30, 0x7C, 0xB8, 0x00,
-				0x11, 0xBB, 0x00, 0x10, 0xCD, 0x10, 0xBA, 0x80,
-				0x00, 0xBD, 0x00, 0x7E, 0xCD, 0x10, 0xEB, 0x07,
-				/*
-				以下程序用于恢复字模，汇编源码:
-				B8 04 11	mov ax,1104
-				B3 00		mov bl,0
-				CD 10		int 10
-				*/
-				0xB8,0x04,0x11,0xb3,0x00,0xcd,0x10,
-				/*ljmp 0000:8200*/
-				0xEA, 0x00, 0x82, 0x00,0x00};
-	struct realmode_regs int_regs = {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
-	memmove((char*)0x7c00,prog,40);
+	struct realmode_regs int_regs = {0,0,0,-1,0,0,0,0x1104,-1,-1,-1,-1,-1,0xFFFF10CD,-1,-1};
+	/*
+		详请请查看以下贴子。
+		http://bbs.znpc.net/viewthread.php?tid=5838&page=12&fromuid=29#pid46781
+		只是希望运行一条简单的 int 软中断指令，则用户应该设置 CS=0xFFFFFFFF, EIP=0xFFFFXXCD。其中 XX 表示中断号。
+	*/
 	if (*arg == 0)//如果没有任何参数，则恢复字模
 	{
-		int_regs.eip = 0x7C18;
-		int_regs.cs = 0;
 		return realmode_run((long)&int_regs);
 	}
 
 	if (!open(arg))
 	{
+		unsigned long long addr = 0LL;
+		if (safe_parse_maxint(&arg,&addr))//这一部份只供测试代码使用。
+		{
+			errnum = 0;
+			int_regs.eip = addr & 0xFFFFFFFF;
+			int_regs.cs = int_regs.es = int_regs.ds =  addr >> 32;
+			int_regs.esp = -1;
+			return realmode_run((long)&int_regs);
+		}
+		errnum = 0;
 		return !printf("Error: open_file\n");
 	}
-
-	unsigned int size = 0;
+	unsigned int size;
+	filepos = 0x59;
+	if (! read((unsigned long long)(unsigned int)&size,4,GRUB_READ) || size != 0x52515350)
+	{
+		close();
+		return !printf("Err: fil");
+	}
 	filepos = 0x1ce;
 	if (! read((unsigned long long)(unsigned int)&size,2,GRUB_READ))
 	{
 		close();
 		return !printf("Error:read1\n");
 	}
-	if (size > 20)
-		size = 20;
+	unsigned char prog[32] = {
+			0xB9, 0x00, 0x00, 0xBA, 0xC0, 0x00, 0xBD, 0x20,
+			0x7C, 0xB8, 0x00, 0x11, 0xBB, 0x00, 0x10, 0xCD,
+			0x10, 0xBA, 0x80, 0x00, 0xBD, 0x00, 0x7E, 0xCD,
+			0x10,
+			/*ljmp 0000:8200*/
+			0xEA, 0x00, 0x82, 0x00,0x00};
+			/*
+			以下程序用于恢复字模，汇编源码:
+			B8 04 11	mov ax,1104
+			B3 00		mov bl,0
+			CD 10		int 10
+			*/
+			//0xB8,0x04,0x11,0xb3,0x00,0xcd,0x10,
+
+	memmove((char*)0x7c00,prog,32);
+	size &= 0xFFFF;
+	if (size > 30)
+		size = 30;
 	int i;
 	for (i=0;i<size;i++)
 	{
-		read((unsigned long long)(unsigned int)(CHAR_BUF1 + (i << 4)),16,GRUB_READ);
-		read((unsigned long long)(unsigned int)(CHAR_BUF2 + (i << 4)),16,GRUB_READ);
+		read((unsigned long long)(unsigned int)(0x7C20 + (i<<4)),16,GRUB_READ);
+		read((unsigned long long)(unsigned int)(0x7E00 + (i<<4)),16,GRUB_READ);
 	}
 	close();
+	*(short*)0x7C01 = size;//设置ECX值;
 	int_regs.eip = 0x7C00;
-	int_regs.cs = 0;
-	int_regs.esp = -1;
-	int_regs.ss = -1;
-	int_regs.ecx= size;
-	int_regs.es = 0;
-	return printf("%x",realmode_run((long)&int_regs));
+	int_regs.cs = int_regs.es = int_regs.ds = 0;
+	int_regs.esp = int_regs.ss = -1;
+	return realmode_run((long)&int_regs);
 }
